6e5f403c87204a236fcf64e3045863536f0fa3ef
AlexLoloMazur <113069138+AlexLoloMazur@users.noreply.github.com>
2022-10-07 20:32:59 -0300

    TP1 EJ6

---
+++ Modulo 2/TP1:/Ejercicio_6.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Dias de la Semana</title>
+</head>
+<body>
+<script>
+var dias = ["Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo"];
+for (i = 0; i < dias.length ; i++) {
+    console.log(dias[i])
+    document.write(dias[i])
+    document.write("</br>")
+}
+</script>
+</body>
+</html>
