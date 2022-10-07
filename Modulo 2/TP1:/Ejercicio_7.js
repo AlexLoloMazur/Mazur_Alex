947934f1b137e0fa390238e6ad60e8d8f2d0ed6a
AlexLoloMazur <113069138+AlexLoloMazur@users.noreply.github.com>
2022-10-07 20:33:21 -0300

    TP1 EJ67

---
+++ Modulo 2/TP1:/Ejercicio_7.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script>
+let dato, resultado;
+//sale un cartel en navegador que hace una pregunta y guarda el contenido ingresado en una variable
+val1 = window.prompt("Introduce tu nombre", "...");
+val2 = window.prompt("Introduce tu apellido", "...");
+//Muestreo en pantalla de las 2 variables ingresadas
+resultado = `Concatenado tu nombre y apellido es: ${val1} ${val2} ` ;
+document.write(resultado);
+</script>
+</body>
+</html>
