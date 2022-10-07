980c3236a45d6d7051673ea8a1304dea0e5a9766
AlexLoloMazur <113069138+AlexLoloMazur@users.noreply.github.com>
2022-10-07 20:29:50 -0300

    TP1 EJ1

---
+++ Modulo 2/TP1:/Ejercicio_1.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ejercicio 11 </title>
+</head>
+<body>
+<script>
+document.writeln("Hola Mundo!");
+document.write("</br>")
+document.writeln("Soy el primer script");
+</script>
+</body>
+</html>
