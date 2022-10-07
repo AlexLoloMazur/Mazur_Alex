1f9b1209314667e40ffc5b4ff0f4927fb5562fee
AlexLoloMazur <113069138+AlexLoloMazur@users.noreply.github.com>
2022-10-07 20:35:30 -0300

    TP1 EJ9

---
+++ Modulo 2/TP1:/Ejercicio_9.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script>
+var vocales = ["A","E","I","O","U"];
+for (i=0; i < vocales.length; i++) {
+document.write(vocales[i], "</br>");
+}
+</script>
+</body>
+</html>
