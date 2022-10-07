5021e795d0bd7a6adfb950dc2363e08d66363deb
AlexLoloMazur <113069138+AlexLoloMazur@users.noreply.github.com>
2022-10-07 20:35:50 -0300

    TP1 EJ10

---
+++ Modulo 2/TP1:/Ejercicio_10.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script>
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+val1 = window.prompt("Introduce el numero de mes que desea visualizar")
+document.write(meses[val1-1]);
+</script>
+</body>
+</html>
