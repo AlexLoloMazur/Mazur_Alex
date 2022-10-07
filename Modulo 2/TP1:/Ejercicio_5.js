328d0f70fa7b093b73ec051abab224d71639fcf3
AlexLoloMazur <113069138+AlexLoloMazur@users.noreply.github.com>
2022-10-07 20:31:54 -0300

    TP1 EJ5

---
+++ Modulo 2/TP1:/Ejercicio_5.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html><body>
+<script type="text/javascript"> 
+//están cargadas 2 veces las variables. en Playground.io toma como error la linea 6
+//No pude saber si es un error en si o no, porque VSC toma bien teniendo esa linea
+// let a, b;
+a = 3; b = 7;
+resultado = a * b;
+document.write(`Variable a contiene ${a} <br> ` );
+document.write(`Variable b contiene ${b} <br> ` );
+//estaba mal cargada la variable resultado
+document.write(`El producto de a por b es ${resultado} `);
+</script>
+<script>
+//entiendo la linea 17 como un consejo
+</script>
+<p>Los puntos de interrupción son fundamentales para programar</p>
+</body>
+</html>
