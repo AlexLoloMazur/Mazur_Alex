ee91568d35fbfcd134489ccb5ebf0ca2b3a5ee1c
AlexLoloMazur <113069138+AlexLoloMazur@users.noreply.github.com>
2022-10-07 20:30:39 -0300

    TP1 EJ2

---
+++ Modulo 2/TP1:/Ejercicio_2.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script>
+//declaro las variables
+let A, B;
+//le asigno valor a cada variable
+A = 10;
+B = 5;
+resultado = A + B;
+//la funcion alert me permite mostrar una miniventana de alerta
+//en el navegador con el contenido que se indique. sin que muestre nada en la pestaña del navegador
+//propiamente dicho
+alert (`La suma de A+B es: ${resultado} ` );
+alert (`La diferencia de A-B es: ${A-B} ` );
+alert (`La multiplicación de AxB es: ${A*B} ` );
+alert (`La división de A/B es: ${resultado=A/B} ` );
+</script>
+</body>
+</html>
